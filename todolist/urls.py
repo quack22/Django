079543671a36caf54e7b3322a238c9a12aff9b5a
Farrel Zandra <farrel.zandra.tif423@polban.ac.id>
2024-05-25 22:07:59 +0700

    developing CRUD for the app

---
+++ todolist/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+
+app_name = 'todolist'
+urlpatterns = [
+    path('', views.index, name='todo-index'),
+    path('add/', views.add, name='todo-add'),
+    path('update/<int:todo_id>', views.update, name='todo-update'),
+    path('delete/<int:todo_id>', views.delete, name='todo-delete'),
+]
--- todolist/views.py
+++ todolist/views.py
@@ -1,3 +1,30 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponseRedirect, get_object_or_404, redirect
+from .models import Todo
+from django.urls import reverse
 
 # Create your views here.
+def index(request):
+    all_todos = Todo.objects.all()
+    context = dict(
+        todos = all_todos
+    )
+    return render(request, 'todolist/index.html', context)
+
+def add(request):
+    new_todo = request.POST['todo']
+    todo = Todo(text = new_todo)
+    todo.save()
+
+    return HttpResponseRedirect(reverse('todo-index'))
+
+def update(request, todo_id):
+    todo = Todo.objects.get(id=todo_id)
+    todo.status = not todo.status
+    todo.save()
+
+    return HttpResponseRedirect(reverse('todo-index'))
+
+def delete(request, todo_id):
+    todo_item = get_object_or_404(todo, pk=todo_id)
+    todo_item.delete()
+    return redirect('todolist::todo_list')
